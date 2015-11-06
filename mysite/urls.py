f77baca20face8986bc7335f7670aae7c56acc87
johnfkraus <johnkraus3@gmail.com>
2015-11-06 16:36:11 -0500

    finished djtutorials ext

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,20 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
 
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'', include('blog.urls')),
+)
+
+"""
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
+
+"""
