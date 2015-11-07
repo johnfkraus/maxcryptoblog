f6f3ffec37afa10c71a27e5d22b87ce6a0742f8b
johnfkraus <johnkraus3@gmail.com>
2015-11-07 11:37:39 -0500

    created requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,11 +79,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cblog',
+        'USER': 'cblogger',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
