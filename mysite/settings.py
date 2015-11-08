d224b1d03e47fd114727e178704b7a192f6d1c93
johnfkraus <johnkraus3@gmail.com>
2015-11-07 20:34:30 -0500

    update for pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,14 +87,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
