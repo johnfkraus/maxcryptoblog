45af364519c0e599a16b1b1f310654456f707a2c
johnfkraus <johnkraus3@gmail.com>
2015-11-09 11:10:50 -0500

    commented out postgres/heroku settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,10 +122,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
