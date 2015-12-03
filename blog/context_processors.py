4f0944f8d093d5017954731fb28210c7448d4e43
johnfkraus <johnkraus3@gmail.com>
2015-12-03 08:59:08 -0500

    fixed spelling errors in comments, tweaked .gitignore

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,10 +2,10 @@ from django.conf import settings  # import the settings file
 
 
 def app_title(request):
-    # return the value you want as a dictionnary. you may add multiple values in there.
+    # return the value you want as a dictionary. you may add multiple values in there.
     return {'APP_TITLE': settings.APP_TITLE}
 
 
 def current_site(request):
-    # return the value you want as a dictionnary. you may add multiple values in there.
+    # return the value you want as a dictionary. you may add multiple values in there.
     return {'current_site': settings.APP_TITLE}
